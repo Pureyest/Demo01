5031b328ae13b91e08fa2b72db45ebd9f8320609
Pureyest <1149715269@qq.com>
2020-09-28 15:49:26 +0800

    first send code

---
+++ demo01.py
@@ -0,0 +1,37 @@
+"""print("hello h&m~") #   字符串
+print(123)
+print(1.23)
+print(True) #   布尔值
+print(())   #   元组
+print([])   #   数组
+print({})   #   字典
+'''
+床前明月光
+疑是地上霜
+'''
+print("hh",2333)
+print("hhh"+"xixixi")
+print("hh"*100)"""
+
+# a=float(input('请输入：'))
+# b=float(input('请输入：'))
+# print("ok",len(a+b))
+# print(type(210))
+# print(type(2.10))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a=str(13)
+# print(type(a))
+
+
+# 通过代码获取两段内容，并计算他们长度的和
+# a="123"
+# print(len(a))
+
+a=input("please input:")
+b=input("please input:")
+print("两端长度为：",len(a+b))
+
